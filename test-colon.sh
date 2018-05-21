4471ce5dc0f7d4fd05329eebbbfe351b741a93bd
d-- <d--@users.noreply.github.com>
2018-05-21 16:11:25 -0400

    Fix issue #1

--- test-colon.sh
+++ test-colon.sh
@@ -4,6 +4,16 @@
 # description string and a bash conditional, evaluates the conditional, and
 # gives some pretty output.
 
+# Now this only works on Linux.
+#
+# In order to support changing directories before sourcing this file (and still
+# knowing who sourced it), we have to go around bash and query the OS to figure
+# out where the parent script lives.  We do this by handling the relative and
+# absolute cases of $0 from the current working directory of the parent shell.
+if [[ ! -z "${BASH_SOURCE[1]}" ]]; then
+  __WHO_SOURCED_ME="$(cd /proc/${PPID}/cwd; readlink -f ${0})"
+fi
+
 function test:() {
   local red=''
   local green=''
@@ -43,7 +53,7 @@ function test:() {
       printf ": (${test_file}:${test_line})\n"
       {
         echo "trap 'printf -- \"--> \${BASH_COMMAND}\n\"; exit' DEBUG"
-        tail -n +${test_line} ${test_file}
+        tail -n +${test_line} ${__WHO_SOURCED_ME}
       } | /bin/bash
     fi
     echo ""
